d74539444c106934675163ecb50087781ae3d7c5
Michelle Han <michellehan2007agt@gmail.com>
2022-10-17 15:56:35 -0700

    navbar

--- script.js
+++ script.js
@@ -65,3 +65,15 @@ function myFunction3() {
     moreText.style.display = "inline";
   }
 }
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
